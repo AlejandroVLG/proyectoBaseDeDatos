cd34ef1155b765ca828361749d2315d637c0205b
Alexlg <Alejandrolg86@gmail.com>
2022-06-17 09:56:20 +0200

    fixing auth problem on ClientsRouter.js

--- views/ClientsRouter.js
+++ views/ClientsRouter.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const auth = require("../config/auth");
+const auth = require("../middlewares/auth");
 const router = express.Router();
 
 const ClientsController = require("../controllers/ClientsController");
 const isAdmin = require("../middlewares/isAdmin");
 
-router.post("/myAccount", ClientsController.showMember);
+router.post("/myAccount", auth, ClientsController.showMember);
 router.get("/membersList", isAdmin, ClientsController.showMembers);
 router.post("/addMember", ClientsController.newMember);
 router.post("/login", ClientsController.memberLogin);
-/* router.put("/modifyProfile", ClientsController.modifyClientProfile);
+/* router.put("/modifyProfile", ClientsController.modifyMemberProfile);
  */
 module.exports = router;
